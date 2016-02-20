32eb2395d5ef1fd829163698e49905daf2357ef0
manue1 <manub1312@gmail.com>
2016-02-19 19:27:36 -0600

    Add Composite bar chart.

--- src/bar/composite.js
+++ src/bar/composite.js
@@ -1,151 +1,162 @@
 import Chart from './../base';
 
 // inspired by https://gist.github.com/mbostock/4b66c0d9be9a0d56484e
-class CalendarGridChart extends Chart{
+class CompositeBarChart extends Chart {
 
-  get chart_options(){
-    var chart = this;
-    return Object.assign(Chart.DEFAULTS, {
-      margin: {top: 30, left: 150, bottom: 0, right: 0},
-      grid_padding: 0.05,
-      parse_date_format: '%Y-%m-%d',
-      display_date_format: '%B %Y',
-      date_attr: 'date',
-      range_attr: undefined,
-      min_range_zero: false,
-      color: '#FFF',
-      extent: []
-    })
+  get chart_options() {
+    return Object.assign({}, Chart.DEFAULTS);
   }
 
-  defineAxes(){
-    var grid_chart = this;
+  afterAxes() {
+    var composite_chart = this;
+    composite_chart.fnLine = d3.svg.line()
+      .interpolate(composite_chart.chart_options.interpolation)
+      .x(function(d) {
+        return line_chart.x_scale(d[line_chart.domain_attr]);
+      })
+      .y(function(d) {
+        return line_chart.y_scale_right(d[line_chart.line_attr]);
+      });
+  }
 
-    // y scale is dependent on number of months.
-    grid_chart.y_axis = d3.svg.axis().orient("left").outerTickSize(0);
-    grid_chart.y_scale = d3.scale.ordinal()
-    grid_chart.svg.append("g")
-        .attr("class", "d3-chart-range d3-chart-axis");
+  defineAxes() {
+    var chart = this;
 
-    grid_chart.x_scale = d3.scale.ordinal()
-      .domain(d3.range(31).map((n)=>{ return n+1; }))
-      .rangeRoundBands([0, grid_chart.width], grid_chart.grid_padding, 0);
-
-    grid_chart.x_axis = d3.svg.axis()
-      .scale(grid_chart.x_scale)
-      .orient("top")
+    // Axes Left
+    chart.y_scale_left = d3.scale.linear()
+      .range([chart.height, 0]);
+    chart.y_axis_left = d3.svg.axis()
+      .scale(chart.y_scale_left)
+      .orient("left")
       .outerTickSize(0);
 
-    // append x axis
-    grid_chart.svg.append("g").attr("class", "d3-chart-domain d3-chart-axis");
+    // Axes Right
+    chart.y_scale_right = d3.scale.linear()
+      .range([chart.height, 0]);
+    chart.y_axis_left = d3.svg.axis()
+      .scale(chart.y_scale_right)
+      .orient("right")
+      .outerTickSize(0);
+
+    chart.x_scale = d3.scale.ordinal()
+      .rangeRoundBands([chart.height, 0], 0.1);
+
+    chart.x_axis = d3.svg.axis()
+      .scale(chart.x_scale)
+      .orient("bottom")
+      .outerTickSize(0)
+      //chart.x_axis.tickFormat(d3.time.format('%b %d at %H'))
+      //chart.x_axis.ticks(d3.time.hour, 12);
+
+    // append axis groups.
+    chart.svg.append("g")
+      .attr("class", "d3-chart-range d3-chart-range-left d3-chart-axis");
+    chart.svg.append("g")
+      .attr("class", "d3-chart-range d3-chart-range-right d3-chart-axis");
+    chart.svg.append("g")
+      .attr("class", "d3-chart-domain d3-chart-axis")
+      .attr("transform", "translate(0, " + (chart.height) + ")");
   }
 
-  afterAxes(){
-    var grid_chart = this;
-    grid_chart.grid_unit_size = grid_chart.width / 31 - grid_chart.grid_padding * grid_chart.width / 30;
+  defineDomain(domain) {
+    var chart = this;
 
-    if (grid_chart.display_date_format) grid_chart.displayDate = d3.time.format(grid_chart.display_date_format);
-
-    if (!grid_chart.toDate && grid_chart.parse_date_format){
-      grid_chart.parseDate = d3.time.format(grid_chart.parse_date_format);
-      grid_chart.toDate = (datum)=>{
-        grid_chart.parseDate(datum[grid_chart.date_attr]);
-      }
-    } else if (!grid_chart.toDate){
-      grid_chart.toDate = (datum)=>{ return datum[grid_chart.date_attr] };
-    }
-
-
-    grid_chart.monthFormat = d3.time.format('%B %Y');
-    grid_chart.toMonthString = (datum)=>{
-      return grid_chart.monthFormat(grid_chart.toDate(datum));
-    }
+    chart.domain = domain;
+    chart.x_scale.domain(domain);
+    chart.svg.select(".d3-chart-domain")
+      .call(chart.x_axis)
+      .selectAll("text")
+      .attr("transform", function() {
+        var elem = this,
+          bbox = elem.getBBox(),
+          middleX = bbox.x + (bbox.width / 2),
+          middleY = bbox.y + (bbox.height / 2);
+        return "rotate(-30," + middleX + "," + middleY + ")";
+      });
   }
 
-  serializeData(data){
-    var grid_chart = this;
-    data.css_class = data.css_class || grid_chart.toClass ? grid_chart.toClass(data) : "";
+  drawBarData(data) {
+    var chart = this;
+    data = chart.serializeBarData(data);
 
-    grid_chart.rangeValue = grid_chart.range_attr ? function(d){ return d[grid_chart.range_attr]; } : grid_chart.rangeValue;
+    chart.y_scale_left.domain(data.range_extent);
+    chart.svg.select(".d3-chart-range").call(chart.y_axis_left);
 
-    data.months = [];
-    if (data.min_range !== undefined && data.max_range !== undefined){
-      data.range = {min: data.min_range, max: data.max_range};
-      data.values.forEach((value)=>{
-        var date = grid_chart.toDate(value),
-          date_s = grid_chart.monthFormat(date);
-        if (data.months.indexOf(date_s) < 0) data.months.push(date_s);
-      });
-    } else {
-      var min_range = Infinity,
-        max_range = -Infinity;
-      data.values.forEach((value)=>{
-        var date = grid_chart.toDate(value),
-          date_s = grid_chart.monthFormat(date),
-          range_value =grid_chart.rangeValue(value);
-        min_range = Math.min(min_range, range_value);
-        max_range = Math.max(max_range, range_value);
-        if (data.months.indexOf(date_s) < 0) data.months.push(date_s);
-      });
-      if (grid_chart.min_range_zero) min_range = Math.min(min_range, 0);
-      data.range = { min: min_range, max: max_range };
-    }
-    data.range.diff = data.range.max - data.range.min;
-
-    data.months = data.months.sort((date_s1, date_s2)=>{
-      var date1 = grid_chart.monthFormat.parse(date_s1),
-        date2 = grid_chart.monthFormat.parse(date_s2);
-      return date1.getTime() - date2.getTime();
+    data.series.forEach(function(series) {
+      var filtered_values = series.values.filter((value) => {
+        return chart.domain.indexOf(value[chart.domain_attr]) < 0;
+      })
+      bars = chart.svg.selectAll(".d3-chart-bar")
+        .data(series.values);
+      chart.applyData(series, bars.enter().append("rect"));
+      chart.applyData(series, bars.transition());
+      bars.exit().remove();
     });
-    return data;
-  };
+  }
 
-  drawData(data){
-    var grid_chart = this;
-    data = grid_chart.serializeData(data);
+  // helper method for drawData
+  applyBarData(series, elements) {
+    var chart = this,
+      series_class = "d3-chart-bar " + series.css_class;
+    elements
+      .attr("class", function(d) {
+        return series_class + " " + d.css_class;
+      })
+      .attr("title", function(d) {
+        return d.title;
+      })
+      .attr("width", chart.x_scale.rangeBand())
+      .attr("x", chart.x_scale(series.title))
+      .attr("height", function(d) {
+        return chart.y_scale(d[chart.bar_attr]);
+      })
+      .attr("y", function(d) {
+        return chart.y_scale(d.cummulative);
+      })
+      .attr('fill', function(d) {
+        return chart.fnColor(d.title);
+      });
+  }
+
+  drawLineData(data) {
+
+    var chart = this;
+    var nested_extent = chart.nestedExtent(data.series, 'values', chart.domain_attr, chart.line_attr);
 
     // calibrate axes
-    var y_axis_height = grid_chart.grid_unit_size * (1 + grid_chart.grid_padding) * data.months.length;
-    grid_chart.y_scale.rangeRoundBands([0, y_axis_height], grid_chart.grid_padding, 0);
-    grid_chart.y_scale.domain(data.months);
-    grid_chart.y_axis.scale(grid_chart.y_scale);
+    bar_chart.y_scale_right.domain([Math.min(0, nested_extent.range_min), nested_extent.range_max]);
+    bar_chart.svg.select(".d3-chart-range-right")
+      .call(bar_chart.y_axis_right);
 
-    grid_chart.svg.select(".d3-chart-range")
-      .call(grid_chart.y_axis);
+    // draw lines
+    var line = g.selectAll(".d3-chart-line")
+      .data(data.series);
 
-    grid_chart.svg.select(".d3-chart-domain").call(grid_chart.x_axis);
-
-   var grid_units = grid_chart.svg.selectAll(".d3-chart-grid-unit")
-        .data(data.values);
-    grid_chart.applyData(data, grid_units.enter().append("rect"));
-    grid_chart.applyData(data, grid_units.transition());
-    grid_units.exit().remove();
+    [line.enter().append('g'), line.transition()].forEach((groups) => {
+      line_chart.applyLineData(groups, data.series);
+    });
+    line.exit().remove();
   }
 
-  // helper method for drawData.
-  applyData(data, elements){
-    var grid_chart = this,
-        series_class = "d3-chart-grid-unit " + data.css_class;
-      elements
-          .attr("class", function(d){ return series_class; })
-          .attr("y", function(d) {
-            var bottom = grid_chart.y_scale(grid_chart.toMonthString(d)),
-              middle = grid_chart.y_scale.rangeBand() / 2 - grid_chart.grid_unit_size / 2;
-            return bottom + middle;
-          })
-          .attr("height", grid_chart.grid_unit_size)
-          .attr("x", function(d) {
-            return grid_chart.x_scale(grid_chart.toDate(d).getDate());
-          })
-          .attr("width", function(d) { return grid_chart.grid_unit_size; })
-          .attr('fill', grid_chart.color)
-          .attr("opacity", function(d) { return grid_chart.applyOpacity(grid_chart.rangeValue(d), data.range);  });
+  applyLineData(groups) {
+    var chart = this;
+    groups
+      .attr('class', function(series) {
+        return "d3-chart-line " + chart.cssClass(series);
+      })
+      .attr("title", function(series) {
+        return series.title;
+      })
+      .append("path")
+      .attr("d", function(series) {
+        return chart.fnLine(series.values.filter((value) => {
+          return chart.domain.indexOf(value[chart.domain_attr]) < 0;
+        }));
+      })
+      .style("stroke", function(series) {
+        return line_chart.fnColor(series.title);
+      });
   }
-
-  applyOpacity(value, range){
-    return Math.max(0, Math.min(1, 1 - (range.max - (value - range.min)) / range.diff));
-  };
-
 }
 
-export default CalendarGridChart;
+export default CompositeBarChart;
