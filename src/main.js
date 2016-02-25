e2b8d2e37d3689ff87f117e33f785996d6c8235e
manue1 <manub1312@gmail.com>
2016-02-25 16:38:47 -0600

    Include composite bar chart in build.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 require('./style.scss');
 
-// import VerticalBarChart from './bar/vertical';
-// import HorizontalBarChart from './bar/horizontal';
 import SplineStackChart from './line/spline_stack';
 import CalendarGridChart from './grid/calendar_grid';
+import CompositeBarChart from './bar/composite';
